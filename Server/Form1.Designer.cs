3468641961099318abd7f3e5f8859ebb4fd2927d
striveandtalent <2823475824@qq.com>
2025-07-08 14:14:21 +0800

    添加项目文件。

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cboUsers = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtSendFile = new System.Windows.Forms.Button();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.btnZD = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地址:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "端口号:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(332, 30);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(170, 25);
+            this.txtPort.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(508, 20);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(147, 41);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "连接";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // cboUsers
+            // 
+            this.cboUsers.FormattingEnabled = true;
+            this.cboUsers.Location = new System.Drawing.Point(751, 32);
+            this.cboUsers.Name = "cboUsers";
+            this.cboUsers.Size = new System.Drawing.Size(153, 23);
+            this.cboUsers.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(685, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "客户端:";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 72);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(925, 193);
+            this.txtLog.TabIndex = 7;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 271);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(925, 193);
+            this.txtMsg.TabIndex = 8;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 487);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(535, 25);
+            this.txtPath.TabIndex = 9;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(571, 479);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(144, 47);
+            this.btnSelectFile.TabIndex = 10;
+            this.btnSelectFile.Text = "选择文件";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtSendFile
+            // 
+            this.txtSendFile.Location = new System.Drawing.Point(738, 479);
+            this.txtSendFile.Name = "txtSendFile";
+            this.txtSendFile.Size = new System.Drawing.Size(144, 47);
+            this.txtSendFile.TabIndex = 11;
+            this.txtSendFile.Text = "发送文件";
+            this.txtSendFile.UseVisualStyleBackColor = true;
+            this.txtSendFile.Click += new System.EventHandler(this.txtSendFile_Click);
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(571, 532);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(144, 47);
+            this.btnSendMessage.TabIndex = 12;
+            this.btnSendMessage.Text = "发送消息";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // btnZD
+            // 
+            this.btnZD.Location = new System.Drawing.Point(738, 532);
+            this.btnZD.Name = "btnZD";
+            this.btnZD.Size = new System.Drawing.Size(144, 47);
+            this.btnZD.TabIndex = 13;
+            this.btnZD.Text = "震动";
+            this.btnZD.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 588);
+            this.Controls.Add(this.btnZD);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.txtSendFile);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboUsers);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cboUsers;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button txtSendFile;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.Button btnZD;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint port = new IPEndPoint(IPAddress.Any, Convert.ToInt32(txtPort.Text));
+                socketWatch.Bind(port);
+                socketWatch.Listen(1000);
+                ShowMsg("监听成功!");
+                await Task.Run(() =>
+                {
+                    Thread th = new Thread(Listen);
+                    th.IsBackground = true;
+                    th.Start(socketWatch);
+                });
+            }
+            catch (Exception ex)
+            {
+                ShowMsg(ex.Message);
+            }
+        }
+        Dictionary<string, Socket> dicSocket = new Dictionary<string, Socket>();
+        private async void Listen(object obj)
+        {
+            Socket socketWatch = obj as Socket;
+            try
+            {
+                while (true)
+                {
+                    Socket socketSend = socketWatch.Accept();
+                    dicSocket.Add(socketWatch.RemoteEndPoint.ToString(), socketSend);
+                    cboUsers.Items.Add(socketSend.RemoteEndPoint.ToString());
+                    ShowMsg($"{socketSend.RemoteEndPoint}连接成功!");
+                    await Task.Run(() =>
+                    {
+                        Thread th = new Thread(Receive);
+                        th.IsBackground = true;
+                        th.Start(socketSend);
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowMsg(ex.Message);
+            }
+        }
+
+        private void Receive(object obj)
+        {
+            StringBuilder sb = new StringBuilder();
+            Socket socketSend = obj as Socket;
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    sb.Append(Encoding.UTF8.GetString(buffer), 0, r);
+                }
+                ShowMsg(sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                ShowMsg(ex.Message);
+            }
+        }
+
+        private void ShowMsg(string str)
+        {
+            txtLog.AppendText(str + "\r\n");
+        }
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cboUsers.SelectedItem != null)
+                {
+                    string str = txtMsg.Text.Trim();
+                    byte[] buffer = Encoding.UTF8.GetBytes(str);
+                    List<byte> list = new List<byte>();
+                    list.Add(0);//文字标识
+                    list.AddRange(buffer);
+                    byte[] newBuffer = list.ToArray();
+                    int r = dicSocket[cboUsers.SelectedItem.ToString()].Send(newBuffer);
+                    if (r != 0)
+                    {
+                        ShowMsg($"{dicSocket[cboUsers.SelectedItem.ToString()].LocalEndPoint}:{str}");
+                    }
+                    else
+                    {
+                        ShowMsg("发送失败!");
+                    }
+                }
+                else
+                {
+                    ShowMsg("未选择客户端");
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowMsg(ex.Message);
+            }
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pfd = new OpenFileDialog();
+            pfd.Title = "强选择要发送的文件";
+            pfd.InitialDirectory = @"C:\Users\zhanglifu\Desktop";
+            pfd.Filter = "文本文件|*.txt|全部文件|*.*";
+            pfd.ShowDialog();
+            txtPath.Text = pfd.FileName;
+        }
+
+        private void txtSendFile_Click(object sender, EventArgs e)
+        {
+            if (cboUsers.SelectedItem != null)
+            {
+                try
+                {
+                    using (FileStream fsRead = new FileStream(txtPath.Text, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] buffer = new byte[1024 * 1024 * 5];
+                        int r = fsRead.Read(buffer, 0, buffer.Length);
+                        List<byte> list = new List<byte>();
+                        list.Add(1);//文件类型
+                        list.AddRange(buffer);
+                    }
+                }
+                catch
+                {
+                    ShowMsg("未选择文件");
+                }
+            }
+            else {
+                ShowMsg("未选择客户端");
+            }
+        }
+    }
+}
